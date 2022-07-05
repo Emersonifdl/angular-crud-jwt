dc622bf42fe0b03af5047f3dcbd8beb3f43a683c
Emerson Lima <emerson.ifdl@gmail.com>
2022-07-05 18:30:11 -0300

    Add initial base

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = JSON.parse(localStorage.getItem('token') as string);
+
+    if (token) {
+      const cloned = request.clone({
+        headers: request.headers.set(
+          'Authorization',
+          'Bearer ' + token.access_token
+        ),
+      });
+
+      return next.handle(cloned);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
